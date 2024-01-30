62f5cceb6108dc65ebd62ad3447a553e779f0a0b
Abs01vte <hammykyle@gmail.com>
2024-01-30 12:40:45 -0700

    Added yearly analyses and a larger selection of symbols. Algorithms for analyzing the trend have been tweaked to better respect the direction and momentum of buying/selling

--- trading_main.py
+++ trading_main.py
@@ -13,68 +13,10 @@ import kaleido
 #from dash import 
 
 # Ticker Symbol and Date Range
-symbols = ["AAPL", "AMD", "AMZN", "ARM", "BITB", "GOOGL", "INTC", "MARA", "MSFT", "ORCL", "SPY", "TSM", "TXN"]
+symbols = ["AAPL", "AMD", "AMZN", "ARM", "BITB", "GOOGL", "INTC", "MARA", "MSFT", "ORCL", "OXY", "SPY", "TSM", "TXN", "TSLA"]
 
 st = dt.datetime(2023, 1, 2)
 en = dt.datetime.today()
-
-#stockdata = pdr.DataReader('AAPL', 'stooq',st,en)
-#stockdata = yf.download(["AAPL", "AMD", "AMZN", "GOOG", "INTC", "ORCL", "TXN"], start="2024-01-02", end="2024-01-24", group_by='ticker', interval="5m")
-#print(stockdata.head())
-
-# Loop through symbols
-"""
-# Save for Yearly Statements
-for x in range(len(symbols)):
-    yearlyHigh = 0
-    stockdaten = pdr.DataReader(symbols[x], 'stooq', st, en)
-    yearlyLow=stockdaten.High.values[x]
-    for y in range(len(stockdaten)):
-        if(stockdaten.High.values[y] > yearlyHigh):
-            yearlyHigh = float(stockdaten.High.values[y])
-        elif(stockdaten.Low.values[y] < yearlyLow):
-            yearlyLow = float(stockdaten.Low.values[y])
-    diffYearly = yearlyHigh-yearlyLow
-    # Reset index to make 'Date' a column
-    stockdaten.reset_index(inplace=True)
-
-    # Create candlestick chart
-    graph = go.Figure(data=[go.Candlestick(x=stockdaten['Date'],
-                                           open=stockdaten['Open'],
-                                           high=stockdaten['High'],
-                                           low=stockdaten['Low'],
-                                           close=stockdaten['Close'])])
-
-    # Calulate Yearly Fibonacci Levels
-    fibLevels = [0.236, 0.382, 0.618, 0.764]
-    yearlyFibPrices=[0,0,0,0]
-    snex=0
-    while(snex < 4):
-        yearlyFibPrices[snex] = yearlyLow + (diffYearly * fibLevels[snex])
-        snex+=1
-    # Add them to the graph
-    graph.add_shape(type="line", y0=yearlyLow, fillcolor="green", y1=yearlyLow, x0=st,x1=en)
-    graph.add_shape(type="line",y0=yearlyFibPrices[0], fillcolor="#34eb77", y1=yearlyFibPrices[0], x0=st,x1=en)
-    graph.add_shape(type="line",y0=yearlyFibPrices[1], fillcolor="#07f2ff", y1=yearlyFibPrices[1], x0=st,x1=en)
-    graph.add_shape(type="line",y0=yearlyHigh-(diffYearly/2), fillcolor="#e834eb", y1=(yearlyHigh-(diffYearly/2)), x0=st,x1=en)
-    graph.add_shape(type="line",y0=yearlyFibPrices[2], fillcolor="#eb9834", y1=yearlyFibPrices[2], x0=st,x1=en)
-    graph.add_shape(type="line",y0=yearlyFibPrices[3], fillcolor="#eb5b34", y1=yearlyFibPrices[3], x0=st,x1=en)
-    graph.add_shape(type="line",y0=yearlyHigh, fillcolor="#b80018", y1=yearlyHigh, x0=st,x1=en)
-    
-
-    # Update layout
-    graph.update_layout(
-        title=f"{symbols[x]} Stock Price Movement",
-        xaxis_rangeslider_visible=False,
-        xaxis_title="Date Range",
-        yaxis_title="Price",
-    )
-    # Show the chart
-    plo.plot(graph)
-    """
-#stockdata = pdr.DataReader('AMD', 'stooq',st,en)
-#stockdata = yf.download(["AAPL", "AMD", "AMZN", "GOOG", "INTC", "ORCL", "TXN"], start="2024-01-02", end="2024-01-24", group_by='ticker', interval="5m")
-yesterday = dt.datetime.today() - timedelta(1)
 
 class Candle:
     def __init__(self, date, open, high, low, close):
@@ -94,7 +36,6 @@ class Order:
         orderReport = pd.DataFrame(data=orderData, index=[0])
         print(orderReport.head())
 
-# Fibonacci Retracement Level Calculation
 
 # trend is expressed 0-2 for 0 = bullish, 1 = bearish, and 2 = sideways
 trend = 0
@@ -105,6 +46,135 @@ def analyzeTrend(val):
         return "Bearish"
     else:
         return "Neutral/Sideways"
+
+
+# Loop through symbols
+
+# Save for Yearly Statements
+
+for x in range(len(symbols)):
+    yearlyHigh = 0
+    stockdaten = pdr.DataReader(symbols[x], 'stooq', st, en)
+    yearlyLow=stockdaten.High.values[x]
+    # Analyze Trend
+    yearlyTrend = 0
+    # Starting with the oldest data in the dataset
+    yearlyIndex = len(stockdaten.Open.values)-1
+    # delta_value is the change in value from open -> close across the examination period
+    yearly_delta_value=0
+    # gap_value is the change in value from close -> open across the examination period
+    yearly_gap_value=0
+
+    ## Gap Value shows institutional sentiment, 
+    ## Delta Value shows retail sentiment
+    for y in range(len(stockdaten)):
+        if(stockdaten.High.values[y] > yearlyHigh):
+            yearlyHigh = float(stockdaten.High.values[y])
+        elif(stockdaten.Low.values[y] < yearlyLow):
+            yearlyLow = float(stockdaten.Low.values[y])
+        if(y<len(stockdaten.Open.values)-1):
+            yearly_gap_value+=stockdaten.Close.values[y+1] - stockdaten.Open.values[y]
+            yearly_delta_value+=stockdaten.Close.values[y]-stockdaten.Open.values[y]
+    diffYearly = yearlyHigh-yearlyLow
+
+    # Reset index to make 'Date' a column
+    stockdaten.reset_index(inplace=True)
+
+    # Create candlestick chart
+    graph = go.Figure(data=[go.Candlestick(x=stockdaten['Date'],
+                                           open=stockdaten['Open'],
+                                           high=stockdaten['High'],
+                                           low=stockdaten['Low'],
+                                           close=stockdaten['Close'])])
+
+    # Calulate Yearly Fibonacci Levels
+    fibLevels = [0.236, 0.382, 0.618, 0.764]
+    yearlyFibPrices=[0,0,0,0]
+    snex=0
+    while(snex < 4):
+        yearlyFibPrices[snex] = yearlyLow + (diffYearly * fibLevels[snex])
+        snex+=1
+    
+    # Expansion being false indicates Compression
+    # Expansion being true indicates that the price will go past the current examination bands
+    expansion = False
+    yearly_delta_percent = yearly_delta_value/(stockdaten.Open.values[len(stockdaten.Open.values)-1])
+    yearly_gap_percent = yearly_gap_value/(stockdaten.Open.values[len(stockdaten.Open.values)-1])
+    
+    if(yearly_delta_value<-0.5 and yearly_gap_percent>0.2):
+        # If the trend is downwards, with several gap up movements, 
+        # but the price of the yearly difference in value between the open of the year and now is in a buying zone
+        # then it is bullish
+        if((yearly_delta_value + stockdaten.Open.values[len(stockdaten.Open.values)-1]) > yearlyFibPrices[0]):
+            trend = 0
+            expansion=True
+        # If there are several gap up movements that result in selling, it is then bearish
+        # Such as institutions covering their shorts with a higher entry price by buying pre- or post-market
+        elif((yearly_delta_value + stockdaten.Open.values[len(stockdaten.Open.values)-1]) < yearlyFibPrices[0]):
+            trend = 1
+    # If there are several gap down movements that result in buying,then it is bullish, and is likely to expand to the upside
+    if(yearly_delta_percent>1 and yearly_gap_percent<-0.2):
+        trend = 0
+        expansion=True
+    # If the change in value is a strong bearish candle, and the gaps confirm, then it is bearish
+    if(yearly_delta_percent < -0.05 and yearly_gap_percent < 0):
+        trend = 1
+    # If the change in value is a strong bullish candle, and the gaps confirm, then it is bullish
+    elif(yearly_delta_percent > 0.01 and yearly_gap_value > 0):
+        trend = 0
+    # If the change in value is neutral/doji candle, and the gaps are neutral or lower, then it is sideways
+    elif(-0.2<yearly_delta_value<0.2 and yearly_gap_percent < 0.01):
+        trend = 2
+    # If the change in value is bearish, but institutional sentiment is up, then it is bullish, and is likely to expand to the upside
+    elif(yearly_delta_percent < -0.1 and yearly_gap_value > 0):
+        trend = 0
+        expansion=True
+    # If the change in value is bullish, but institutional sentiment is down, then it is bearish, and is likely to expand to the downside
+    elif(yearly_delta_percent > 0.10 and yearly_gap_value < 0):
+        trend = 1
+        expansion=True
+    # If all else fails, it is sideways therefore
+    else:
+        trend = 2
+    
+    # Add them to the graph
+    graph.add_hline(y=yearlyLow)
+    graph.add_hline(y=yearlyFibPrices[0])
+    graph.add_hline(y=yearlyFibPrices[1])
+    graph.add_hline(y=(yearlyHigh - (diffYearly/2)))
+    graph.add_hline(y=yearlyFibPrices[2])
+    graph.add_hline(y=yearlyFibPrices[3])
+    graph.add_hline(y=yearlyHigh)    
+
+
+
+    # Update layout
+    graph.update_layout(
+        title=f"{symbols[x]} Stock Price Movement",
+        xaxis_rangeslider_visible=False,
+        xaxis_title="Date Range",
+        yaxis_title="Price",
+        annotations=[
+            go.layout.Annotation(
+                text=f"Over the examination period:<br>The trend for {symbols[x]} is {analyzeTrend(trend)}<br>Institutional buying: {yearly_gap_value:10.2f} or {yearly_gap_percent*100:10.2f}%<br>Retail buying: {yearly_delta_value:10.2f} or {yearly_delta_percent*100:10.2f}%<br>Buying zones: <br>Long- {yearlyFibPrices[0]:10.2f} to {yearlyFibPrices[1]:10.2f} and<br>Short- {yearlyFibPrices[3]:10.2f}   to    {yearlyFibPrices[2]:10.2f}",
+                align='left',
+                showarrow=False,
+                xref='paper',
+                x=0.47,
+                y=yearlyHigh+(yearlyHigh*0.01)+0.5,
+                font=(dict(size=12)),
+                bordercolor='yellow',
+                borderwidth=2
+            )
+        ],
+    )
+    # Show the chart
+    plo.plot(graph)
+    
+
+yesterday = dt.datetime.today() - timedelta(1)
+
+
 # Daily Highs and Lows are calculated in realtime based on the newest 5min charts
 dailyHigh = 0
 dailyLow = 0
@@ -152,14 +222,15 @@ for x in range(len(symbols)):
             trendHigh = trendData.High.values[index]
         delta_value+=trendData.Close.values[index] - trendData.Open.values[index]
         if(index<3):
-            gap_value+=trendData.Open.values[index] - trendData.Close.values[index + 1]
-            delta_value+=trendData.Open.values[index] - trendData.Close.values[index+1]
+            gap_value+=trendData.Close.values[index + 1]- trendData.Open.values[index]
+            delta_value+=trendData.Close.values[index] - trendData.Open.values[index]
         index-=1
     
     # diffTrend is the absolute value of the Fibonacci range for the examination period
     # used for calculating the Fibonacci retracement levels
     diffTrend=trendHigh-trendLow
-    # 
+    # Expansion being false indicates Compression
+    # Expansion being true indicates that the price will go past the current examination bands
     expansion = False
     
     # If there are several gap up days that result in selling, it is then bearish, and is likely to expand to the downside
